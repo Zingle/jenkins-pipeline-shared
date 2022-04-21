e1f9fdea16fb3060afd6f8e390bdb6b1ed668c85
Andrew Velis <avelis@users.noreply.github.com>
2022-04-21 11:29:27 -0500

    Reworks call method to add channel, type & build arguments

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -3,36 +3,29 @@
 /**
  * Send notifications based on build status string
  */
-def call(String buildStatus = 'STARTED') {
+def call(String channel = '#zingle-deployment-notifs', String type = 'BUILD', String status = 'STARTED') {
   // build status of null means successful
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+  status =  status ?: 'SUCCESSFUL'
 
   // Default values
   def color = 'RED'
   def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+  def summary = """${type} ${status}: Job '`${env.JOB_NAME}` [${env.BUILD_NUMBER}]' 
+  (${env.BUILD_URL})
+
+  ${env.CHANGE_TITLE}
+  (${env.CHANGE_URL})"""
 
   // Override default values based on build status
-  if (buildStatus == 'STARTED') {
+  if (status == 'STARTED') {
     color = 'YELLOW'
     colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
+  } else if (status == 'SUCCESSFUL') {
     color = 'GREEN'
     colorCode = '#00FF00'
   }
 
   // Send notifications
-  slackSend (color: colorCode, message: summary)
+  slackSend (channel: channel, color: colorCode, message: summary)
 
-  hipchatSend (color: color, notify: true, message: summary)
-
-  emailext (
-      to: 'bitwiseman@bitwiseman.com',
-      subject: subject,
-      body: details,
-      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
-    )
 }
